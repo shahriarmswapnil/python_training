dcd4cc7375cf14ac2cf94fd5a43421f3c64d057b
Shahriar Swapnil <shahriarmswapnil@gmail.com>
2024-04-21 19:57:44 -0300

    dictionary added

--- first_program/main.py
+++ first_program/main.py
@@ -1,2 +1,28 @@
 import pandas
 print('trying to see if this will work or not')
+
+menus=[
+    ['Alu Bhaji', 'Porota','Shemai'],
+    ['Kachchi', 'Tehari'],
+    ['Mach Bhat','Shak Bhaji','Korolla Bhaji']
+
+]
+
+for items in menus:
+    for item in items:
+        print(item)
+
+
+#Dictionary example
+
+menus={
+    'breakfast':     ['Alu Bhaji', 'Porota','Shemai'],
+    'lunch':['Kachchi', 'Tehari'],
+    'dinner':['Mach Bhat','Shak Bhaji','Korolla Bhaji']
+
+}
+
+print(menus['lunch'])
+
+for name, menu in menus.items():
+    print(name, ":", menu)
